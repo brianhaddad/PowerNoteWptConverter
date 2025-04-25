5791a39e7da2aeebfe6889b4fdca0a4333105359
Brian Haddad <brianhaddad@gmail.com>
2025-04-25 13:19:08 -0700

    Completed first functioning converter.
    You can now publish a utility that will allow you to drag and drop WPT files onto the executable and convert any files you pass in.
    
    The conversion makes a lot of assumptions and already some inconsistencies have surfaced. It would be nice to know more about what the headers says and how tabs work (the control characters do not seem to be standard, but perhaps I just don't understand them very well).
    
    This only works with plaintext and will choke on formatting characters. Or rather, it will spit unrecognized formatting characters out into the final txt file, which will just produce garbage. I may attempt a rich text converter someday but for now this does everything I need it to do.

--- PowerNoteConverter/WptConverter/Outputters/WptToTxt.cs
+++ PowerNoteConverter/WptConverter/Outputters/WptToTxt.cs
@@ -2,10 +2,162 @@
 {
     public class WptToTxt
     {
-        //TODO: need a system for crawling through the file byte by byte.
-        //We will have a list of control codes (that are not standard ANSI or UTF-8)
-        //and if we don't encounter a control code we can convert the byte to UTF-8
-        //but if it's a control code we will need to look a head to see what other
-        //control codes are present in order to determine what character to return.
+        private readonly List<byte> _headerBytes = [];
+        public byte[] HeaderBytes => _headerBytes.ToArray();
+
+        public bool EofError { get; private set; } = false;
+
+        //Some of these are pretty clear from my test files, others (especially anything related to tabs)
+        //are difficult to be sure of. This will require some additional testing and refining I think.
+        private const byte CARRIAGE_RETURN = 0xBD;
+        private const byte LINE_FEED = 0x02;
+        private const byte LINE_BREAK = 0xDB;
+        private const byte UNKNOWN_TAB_INSTRUCTION = 0xBC;
+        private const byte POSSIBLE_TAB_EXECUTE = 0x01;
+        private const byte NEW_LINE_TAB_START = 0xDD;
+        private const byte CENTS_BYTE = 0x20;
+        private const byte SPACE = 0x00;
+        private const byte EOF = 0xDE;
+        private const char CENTS_CHAR = (char)0xA2; //Cents symbol
+
+        //TODO: someday I can refactor this into different parts.
+        //For now it is all one class for simplicity.
+
+        public void Process(string wptFilePath, string txtOutputPath)
+        {
+            List<byte> bytes = [];
+
+            try
+            {
+                using (FileStream fs = new FileStream(wptFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    int byteRead;
+                    while ((byteRead = fs.ReadByte()) != -1)
+                    {
+                        bytes.Add((byte)byteRead);
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+
+            var resultingCharacters = ProcessBytes(bytes.ToArray());
+
+            File.WriteAllLines(txtOutputPath, resultingCharacters);
+        }
+
+        private string[] ProcessBytes(byte[] bytes)
+        {
+            List<string> lines = [];
+
+            int i = 0;
+
+            //The header is terminated by the first instance of CARRIAGE_RETURN
+            while (bytes[i] != CARRIAGE_RETURN)
+            {
+                _headerBytes.Add(bytes[i]);
+                i++;
+            }
+
+            //Advance beyond the initial carriage return.
+            i++;
+
+            var line = "";
+
+            while (i < bytes.Length)
+            {
+                switch(bytes[i])
+                {
+                    //Mid paragraph line breaks are replaced with a space.
+                    case LINE_BREAK:
+                        i++;
+                        if (bytes[i] == CARRIAGE_RETURN)
+                        {
+                            line += " ";
+                            i++;
+                        }
+                        break;
+
+                    //Actual line feeds are replaced with a new line.
+                    //These can consist of multiple line feeds followed by a single carriage return.
+                    //I found one instance where the carriage return was preceeded by what I assume
+                    //is a tab caracter. Other tests with tab did not yield the same result.
+                    case LINE_FEED:
+                        while (bytes[i] == LINE_FEED)
+                        {
+                            lines.Add(line);
+                            line = "";
+                            i++;
+                            if (bytes[i] == NEW_LINE_TAB_START)
+                            {
+                                line += "\t";
+                                i++;
+                            }
+                        }
+                        if (bytes[i] == CARRIAGE_RETURN)
+                        {
+                            i++;
+                        }
+                        break;
+
+                    //Not sure how tab works exactly. Can modify after more testing.
+                    case UNKNOWN_TAB_INSTRUCTION:
+                        i++;
+                        if (bytes[i] == POSSIBLE_TAB_EXECUTE)
+                        {
+                            line += "\t";
+                            i++;
+                        }
+                        break;
+
+                    case EOF:
+                        lines.Add(line);
+                        line = "";
+                        i++;
+                        if (i < bytes.Length)
+                        {
+                            //This should not happen since this character should mark the end of the file.
+                            //We can assume there has been an error or we can keep going. Probably best to
+                            //store the remaining data in an overflow of some kind?
+                            //For now we will ignore the discrepancy and keep processing the data.
+                            EofError = true;
+                        }
+                        break;
+
+                    //The rest are just characters that for one reason or another are encoded
+                    //differently from expected.
+
+                    case CENTS_BYTE:
+                        line += CENTS_CHAR;
+                        i++;
+                        break;
+
+                    case SPACE:
+                        line += " ";
+                        i++;
+                        break;
+
+                    //Finally, most of the remaining characters should just be plain old ASCII.
+                    default:
+                        line += (char)bytes[i];
+                        i++;
+                        break;
+                }
+            }
+
+            //Though the line should be empty, if it's not we will add the final line to the lines list.
+            if (line != "")
+            {
+                lines.Add(line);
+            }
+
+            return lines.ToArray();
+        }
     }
 }
---
+++ PowerNoteConverter/WptToTxtUtility/Program.cs
@@ -0,0 +1,52 @@
+﻿using WptConverter.Outputters;
+
+Console.WriteLine("Running file conversions...");
+
+var inputFilePaths = Environment.GetCommandLineArgs()
+    .Where(x => Path.GetExtension(x).Equals(".WPT", StringComparison.CurrentCultureIgnoreCase))
+    .ToArray();
+
+if (inputFilePaths.Length > 0)
+{
+    Console.WriteLine("Input file paths: ");
+    foreach (var path in inputFilePaths)
+    {
+        Console.WriteLine($"\t{path}");
+    }
+
+    Console.WriteLine("Outputs: ");
+    foreach (var path in inputFilePaths)
+    {
+        var folder = Path.GetDirectoryName(path);
+        var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(path);
+
+        WptToTxt wptToTxt = new();
+        var output = Path.Combine(folder, $"{fileNameWithoutExtension}.txt");
+        wptToTxt.Process(path, output);
+        Console.WriteLine($"\t{output}");
+        Console.WriteLine($"Header Bytes: {string.Join(" ", wptToTxt.HeaderBytes.Select(x => x.ToString("X2")))}");
+        if (wptToTxt.EofError)
+        {
+            Console.WriteLine("EOF Error. Somehow reached the EOF character before the actual end of the bytestream.");
+        }
+    }
+}
+
+#if DEBUG
+//Note: this is just local test code. Feel free to change the path values or play around with the output.
+Console.WriteLine("Begin test.");
+var inputPath = @"C:\Users\brian\Dropbox\projects\bit_banging\WPT\LONGTEST.WPT";
+var outputPath = @"C:\Users\brian\Dropbox\projects\bit_banging\WPT\LongTestOutput.txt";
+WptToTxt converter = new();
+converter.Process(inputPath, outputPath);
+Console.WriteLine("Done.");
+Console.WriteLine("Header bytes:");
+Console.WriteLine(string.Join(" ", converter.HeaderBytes.Select(x => x.ToString("X2"))));
+if (converter.EofError)
+{
+    Console.WriteLine("EOF Error. Somehow reached the EOF character before the actual end of the bytestream.");
+}
+#endif
+
+Console.WriteLine("Done. Press enter to close the window.");
+Console.ReadLine();
