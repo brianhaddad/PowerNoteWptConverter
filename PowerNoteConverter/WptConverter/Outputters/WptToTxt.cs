c68dca83f4e917128d5b559ecaebcbe528ea31b0
Brian Haddad <brianhaddad@gmail.com>
2025-04-24 22:27:15 -0700

    Initial upload of the VisualStudio project with a placeholder class file.

---
+++ PowerNoteConverter/WptConverter/Outputters/WptToTxt.cs
@@ -0,0 +1,11 @@
+﻿namespace WptConverter.Outputters
+{
+    public class WptToTxt
+    {
+        //TODO: need a system for crawling through the file byte by byte.
+        //We will have a list of control codes (that are not standard ANSI or UTF-8)
+        //and if we don't encounter a control code we can convert the byte to UTF-8
+        //but if it's a control code we will need to look a head to see what other
+        //control codes are present in order to determine what character to return.
+    }
+}
